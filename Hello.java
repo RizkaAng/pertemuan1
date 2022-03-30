99d5a5a27f61bc27d588a1cb31feeb73e1596410
RizkaAng <rizkaang01b@gmail.com>
2022-03-30 09:06:47 +0700

    Hasil Compile

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-public class Hello{
-	public static void main(string [] args){
-		system.out.prinln("Hello world !!");
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
 	}
 }
